7789d4fc3decde9f022ab8aca2dfce3c23d27c3f
Lucas Ventura <lnventura@hotmail.com>
2017-07-30 10:20:00 -0300

    closes #1 Update response with project details to send linkSearchToken

--- app/WakandaProjectStorage.js
+++ app/WakandaProjectStorage.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var redis = require('./RedisConnector');
+var CryptoJS = require('crypto-js');
+var AppNameGenerator = require('./utils/AppNameGenerator');
 
 class WakandaProjectStorage {
 
@@ -13,8 +15,25 @@ class WakandaProjectStorage {
     }
 
     fetchProjects(email, callback) {
-        redis.get(email, function (error, projects) {
-            callback.call(this, projects);
+        let generateTokenSearchAPI = function (project) {
+            let filter = JSON.stringify({client: "*"});
+            let token = CryptoJS.AES.encrypt(filter, project.decryptKey, {mode: CryptoJS.mode.CTR}).toString();
+            token = token.replace("+", "%2B");
+            return token;
+        };
+
+        let buildLinkAPISearch = function(project) {
+            let token = generateTokenSearchAPI(project);
+            let appName = AppNameGenerator.buildAppName(project.company, project.name);
+            return AppNameGenerator.buildAppUrl(appName) + "/listStatistics?payload=" + token;
+        };
+
+        redis.get(email, function (error, projectsJsonStringified) {
+            let projectsArray = JSON.parse(projectsJsonStringified);
+            projectsArray.forEach(function(project) {
+                project.linkSearchAPI = buildLinkAPISearch(project);
+            });
+            callback.call(this, JSON.stringify(projectsArray));
         });
     }
 
